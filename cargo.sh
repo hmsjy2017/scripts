8dadb8a2a468e18e4f5f44926d4bdc868a7afa19
Tony <1462668901@qq.com>
2021-08-13 18:01:47 +0800

    Create cargo.sh

---
+++ cargo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cat >> ~/.cargo/config <<EOF
+[source]
+
+[source.mirror]
+registry = "https://mirrors.sjtug.sjtu.edu.cn/git/crates.io-index/"
+
+[source.crates-io]
+replace-with = "mirror"
+EOF  
